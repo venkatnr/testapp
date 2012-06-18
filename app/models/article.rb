fe3ba34c7f8197430bb87a3e37de44a4b4cade7a
venkatnr <oneguyvenkat007@gmail.com>
2012-06-18 18:09:49 +0530

    modification on view part

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ActiveRecord::Base
 belongs_to :user
 has_many :comments
   attr_accessible :description, :title
+  
+  validates :title,  :presence => true
+  validates :description,  :presence => true
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
   attr_accessible :commentonarticle, :name
+  validates :name,  :presence => true
+  validates :commentonarticle,  :presence => true
 end
